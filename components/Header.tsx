6ebe161fac5e1db9deb6e4854cf94f6e9a70dbd9
Robin Ruf <robin.ruf@hotmail.com>
2022-09-01 00:30:28 +0200

    Did some style changes;

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import NavButton from './NavButton'
 import { Bars3BottomRightIcon } from '@heroicons/react/24/solid'
+import { useAddress, useDisconnect } from '@thirdweb-dev/react'
 
 function Header() {
+    const address = useAddress();
+    const disconnect = useDisconnect();
+
   return (
     <header className='grid grid-cols-2 md:grid-cols-5 justify-between items-center p-5'>
         <div className='flex items-center space-x-2'>
            <img className="rounded-full h-20 w-20" src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/2147555239/settings_images/1KukQcMXRFOIMAfrhFv7_youtube-watermark_4x-ranks.png" alt="" /> 
         <div>
             <h1 className='text-lg text-white font-bold'>Lottery</h1>
-            <p className='text-sm text-emerald-500 turncate'>User...</p>
+            <p className='text-sm text-emerald-500 turncate'>Wallet: {address?.substring(0,5)}...{address?.substring(address.length, address.length - 5)}</p>
         </div>
     </div>
 
     <div className='hidden md:flex md:col-span-3 items-center justify-center rounded-md'>
         <div className='bg-[#0A1F1C] p-4 space-x-2'>
             <NavButton isActive title='Buy Tickets' />
-            <NavButton title='Logout' />
+            <NavButton onClick={disconnect} title='Logout' />
         </div>
     </div>
 
@@ -24,7 +28,7 @@ function Header() {
         <Bars3BottomRightIcon className='h-8 w-8 mx-auto text-white cursor-pointer' />
         
         <span className='md:hidden'>
-            <NavButton title='Logout' />
+            <NavButton onClick={disconnect} title='Logout' />
         </span>
     </div>
     </header>
