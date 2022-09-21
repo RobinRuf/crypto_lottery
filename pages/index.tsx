6e2f53670df04dc2717868a2d01103452134f141
Robin Ruf <robin.ruf@hotmail.com>
2022-09-22 00:21:54 +0200

    Implemented a lot of stuff like show bought tickets, show button if you won and more;

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,23 @@ import Header from '../components/Header'
 import { useContract, useMetamask, useDisconnect, useAddress, useContractData, useContractCall } from '@thirdweb-dev/react'
 import Login from '../components/Login'
 import Loading from '../components/Loading'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ethers } from 'ethers'
 import { currency } from '../constants'
 import CountdownTimer from '../components/CountdownTimer'
+import toast from 'react-hot-toast'
 
 const Home: NextPage = () => {
   const [quantity, setQuantity] = useState<number>(1);
   const address = useAddress();
+  const [userTickets, setUserTickets] = useState(0);
   const { contract, isLoading } = useContract(process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS);
   const { data: expiration } = useContractData(contract, 'expiration');
   const { data: ticketPrice } = useContractData(contract, 'ticketPrice');
   const { data: ticketCommission } = useContractData(contract, 'ticketCommission');
+  
+  // Get all sold tickets
+  const { data: tickets } = useContractData(contract, "getTickets");
 
   // get remaining Tickets from Contract
   const { data: remainingTickets } = useContractData(
@@ -29,7 +34,71 @@ const Home: NextPage = () => {
     "CurrentWinningReward"
   );
 
-  // First Loader and then check if already login to prevent page from flicking...
+  // get all Tickets of the User
+  useEffect(() => {
+    if (!tickets) return;
+
+    const totalTickets: string[] = tickets;
+
+    const amountOfUserTickets = totalTickets.reduce((total, ticketAddress) => (ticketAddress === address ? total + 1 : total), 0);
+
+    setUserTickets(amountOfUserTickets);
+  }, [tickets, address])
+
+  // Buy Tickets
+  const { mutateAsync: BuyTickets } = useContractCall(contract, "BuyTickets");
+
+  // get Winnings for Winner
+  const { data: winnings } = useContractData(contract, "getWinningsForAddress", address);
+
+  // withdraw winnings
+  const { mutateAsync: WithdrawWinnings } = useContractCall(contract, "WithdrawWinnings");
+
+  const handleClick = async () => {
+    if (expiration) return;
+    if (!ticketPrice) return;
+
+    const notification = toast.loading('Buying your tickets...');
+
+    try {
+      const data = await BuyTickets([
+        {
+          value: ethers.utils.parseEther(
+            (Number(ethers.utils.formatEther(ticketPrice)) * quantity).toString()
+          ),
+        },
+      ]);
+
+      toast.success('Tickets purchased successfully', {
+        id: notification,
+      });
+    } catch(err) {
+      toast.error('Oops, something went wrong', {
+        id: notification,
+      });
+
+      console.error('contract all failure', err);
+    }
+  }
+
+  const onWithdrawWinnings = async () => {
+    const notification = toast.loading("Withdrawing winnings...");
+
+    try {
+      const data = await WithdrawWinnings([{}]);
+
+      toast.success("Winnings withdrawn successfully!", {
+        id: notification,
+      })
+    } catch(err) {
+      toast.error("Oops, something went wrong! Try again.", {
+        id: notification,
+      })
+      console.error("contract call failure", err);
+    }
+  }
+
+  // First show Loader and then check if already login to prevent page from flicking...
   if (isLoading) return <Loading />
   if (!address) return <Login />
 
@@ -40,7 +109,19 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div className='flex-1'>
       <Header />
+
+      {winnings > 0 && (
+        <div className='max-w-md md:max-w-2xl lg:max-w-4xl mx-auto mt-5'>
+          <button onClick={onWithdrawWinnings} className='p-5 bg-gradient-to-b from-orange-500 to-emerald-600 animate-pulse text-center rounded-xl w-full'>
+            <p className='font-bold'>Winner Winner Chicken Dinner!</p>
+            <p>Total Winnings: {ethers.utils.formatEther(winnings.toString())}{" "}{currency}</p>
+            <br />
+            <p className='font-semibold'>Click Here to Withdraw</p>
+          </button>
+        </div>
+      )}
 
       {/* Draw Box */}
       <div className='space-y-5 md:space-y-0 m-5 md:flex md:flex-row items-start justify-center md:space-x-5'>
@@ -63,6 +144,8 @@ const Home: NextPage = () => {
           </div>
         </div>
 
+        {/** Buy Tickets Box */}
+      <div>
         <div className='stats-container space-z-2'>
           <div className='stats-container'>
             <div className='flex justify-between items-center text-white pb-2'>
@@ -91,24 +174,46 @@ const Home: NextPage = () => {
                 <p>TBC</p>
               </div>
             </div>
-
             <button 
+              onClick={handleClick} 
               disabled={expiration?.toString() < Date.now().toString() || remainingTickets?.toNumber() === 0} 
               className='mt-5 w-full bg-gradient-to-br from-orange-500 to-emerald-600 px-10 
               py-5 rounded-md text-white shadow-xl disabled:from-gray-600 disabled:to-gray-600 
-              disabled:text-gray-100 disabled:cursor-not-allowed'>
-                Buy tickets
+              disabled:text-gray-100 disabled:cursor-not-allowed font-semibold'>
+                Buy {quantity} Tickets for{" "} {ticketPrice && 
+                Number(ethers.utils.formatEther(ticketPrice.toString())) * quantity} {" "} {currency}
             </button>
           </div>
-
+          {userTickets > 0 && (
+            <div className='stats'>
+              <p className='text-lg mb-2'>You have {userTickets} Tickets in this draw</p>
+              <div className='flex max-w-sm flex-wrap gap-x-2 gap-y-2'>
+                {Array(userTickets).fill("").map((_, index) => (
+                  <p key={index} 
+                    className='text-emerald-300 h-20 w-12 bg-emerald-500/30 rounded-lg 
+                    flex flex-shrink-0 items-center justify-center text-xs italic'
+                  >
+                    {index + 1}
+                  </p>
+                ))}
+              </div>
+            </div>
+          )}
         </div>
       </div>
-
-      {/** Buy Tickets Box */}
-      <div>
-
-      </div>
     </div>
+    {/* Footer / Disclaimer */}
+    <div className='border-t border-emerald-500/20 flex items-center text-white justify-between p-5'>
+      <p className='text-xs text-emerald-900 pl-5'>
+        DISCLAIMER: Gambling is only allowed from the age of 18! With every 
+        transaction on this platform, the user confirms to be of legal age. 
+        Gambling can be addictive. If you suffer from gambling addiction, 
+        please contact a local counseling center. This platform does not assume any liability. 
+        A refund is not possible.
+      </p>
+    </div>
+    </div>
+  </div>
   )
 }
 
