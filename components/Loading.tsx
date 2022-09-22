e7d6608e431c69e1630e54203e22cd796725ad59
Robin Ruf <robin.ruf@hotmail.com>
2022-09-23 00:54:14 +0200

    Redesigned Index and Loading Screen; Added a few things to Index; Did some UI changes;

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -3,10 +3,10 @@ import PropagateLoader from 'react-spinners/PropagateLoader'
 
 function Loading() {
   return (
-    <div className='bg-[#091B18] h-screen flex flex-col items-center justify-center'>
-      <div className='flex items-center space-x-2 mb-10'>
-        <img className='rounded-full h-20 w-20' src='https://i.imgur.com/4h7mAu7.png' alt='' />
-        <h1 className='text-lg text-white font-bold'>Loading the Lottery</h1>
+    <div className='bg-gradient-to-b from-[#5068fc] to-[#011570] h-screen flex flex-col items-center justify-center'>
+      <div className='flex flex-col sm:flex-row items-center mx-auto mb-10'>
+        <img className='h-56 w-56 my-10 sm:mr-5' src={"../sapphire.png"} alt='' />
+        <h1 className='text-lg text-white font-bold sm:ml-5'>Sapphire Draw</h1>
       </div>
       <PropagateLoader color='white' size={30} />
     </div>
--- components/Login.tsx
+++ components/Login.tsx
@@ -5,13 +5,22 @@ function Login() {
     const connect = useMetamask();
 
   return (
-    <div className='bg-[#091B18] min-h-screen flex flex-col text-center items-center justify-center'>
+    <div className='bg-gradient-to-b from-[#5068fc] to-[#011570] min-h-screen flex flex-col text-center items-center justify-center'>
         <div className='flex flex-col items-center mb-10'>
-            <img className='rounded-full h-56 w-56 mb-10' src='https://i.imgur.com/4h7mAu7.png' alt='' />
-            <h1 className='text-6xl text-white font-bold'>THE PAPAFAM DRAW</h1>
-            <h2>Get Started By logging in with your Metamask</h2>
+            <img className='h-56 w-56 my-10' src={"../sapphire.png"} alt='' />
+            <h1 className='md:text-6xl sm:text-5xl text-4xl text-white font-bold'>SAPPHIRE DRAW</h1>
+            <h2 className='md:text-md text-sm text-white'>Your Polygon (MATIC) - Crypto Lottery on the <span className='underline'>Ethereum Blockchain</span></h2>
 
-            <button onClick={connect} className='bg-white px-8 py-5 mt-10 rounded-lg shadow-lg font-bold'>Connect with Metamask</button>
+            <button onClick={connect} className='bg-gradient-to-br from-[#5068fc] to-[#011570] text-white px-8 py-5 mt-10 rounded-lg shadow-lg font-bold'>Connect with Metamask</button>
+            <a href='https://myterablock.medium.com/how-to-create-or-import-a-metamask-wallet-a551fc2f5a6b' target='_blank' className='text-white mt-10 underline'>How to create a Metamask Wallet?</a>
+
+            {/* Reasons for Sapphire */}
+            <div className='flex flex-col border-t border-gray-500 mx-5 items-center text-white justify-between p-5 mt-10 w-full'>
+              <h4 className='text-white text-lg sm:text-xl md:text-2xl  items-center mb-5 underline'>Reasons for Sapphire</h4>
+              <p className='text-white text-md italic mb-2 font-thin'>Lowest Service Fees (5%)</p>
+              <p className='text-white text-md italic mb-2 font-thin'>Very Low Gas Fees (Polygon Mainnet)</p>
+              <p className='text-white text-md italic font-thin'>Beautiful and Clean UI</p>
+            </div>
         </div>
     </div>
   )
