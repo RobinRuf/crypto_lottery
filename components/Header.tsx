2f6b92b675bebe08ddb0d156140653a980b6733f
Robin Ruf <robin.ruf@hotmail.com>
2022-09-25 00:34:41 +0200

    Changed Font for brand;

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,12 +10,12 @@ function Header() {
   return (
     <header className='grid grid-cols-2 md:grid-cols-5 justify-between items-center p-5'> 
     <div>
-        <h1 className='text-lg text-white font-bold md:hidden'>Sapphire Draw</h1>
+        <h1 className='text-3xl text-white font-bold font-brand md:hidden'>Sapphire Draw</h1>
         <p className='text-sm text-white turncate'>Wallet: {address?.substring(0,5)}...{address?.substring(address.length, address.length - 5)}</p>
     </div>
 
     <div className='items-center justify-center hidden md:flex md:col-span-3'>
-    <h1 className='text-lg text-white font-bold'>Sapphire Draw</h1>
+    <h1 className='text-5xl text-white font-bold font-brand'>Sapphire Draw</h1>
     </div>
 
     {/* <div className='hidden md:flex md:col-span-3 items-center justify-center rounded-md'>
